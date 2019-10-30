fe9fd628e75796b57c330f6541b9ec095ae49e67
ALIAKSEI PALIANSKI <itpolalex@gmail.com>
2019-10-30 15:55:25 +0300

    1

--- bot-agent/bot/core.go
+++ bot-agent/bot/core.go
@@ -2,20 +2,31 @@ package bot
 
 import (
 	"fmt"
-	"os"
+	"strings"
 )
+
+type Bot struct {
+	BotEnglish
+	BotRussian
+}
 
 func Start() {
 	fmt.Println("Input English for english bot or Введите Russian для русского бота.")
-	var language string
-	_, err := fmt.Scanln(&language)
-	if err != nil {
-		fmt.Print("Something wrong:", err)
-		os.Exit(1)
+	language := ""
+Begin:
+	for {
+		_, err := fmt.Scanln(&language)
+		if err != nil {
+			fmt.Print("Wrong input:", err, "\n")
+			goto Begin
+		}
+		switch {
+		case strings.Contains("engenglishутпутпдшыр", strings.ToLower(language)):
+			fmt.Println("gotcha")
+			var bot Sayer = BotEnglish{}
+			bot.SayHello()
+			bot.SayTime()
+		}
+		fmt.Println("Try again/ Попробуйте снова")
 	}
-	for language != "" {
-
-	}
-	fmt.Println(language)
-
 }
--- bot-agent/bot/interface.go
+++ bot-agent/bot/interface.go
@@ -1,9 +1,30 @@
 package bot
 
+import (
+	"fmt"
+)
+
 type Sayer interface {
 	SayHello()
-	Saytime()
-	SayDate()
-	SayDay()
-	SayBye()
+	SayTime()
+	// SayDate()
+	// SayDay()
+	// SayBye()
 }
+
+func (b Bot) SayHello() {
+	fmt.Println("Hello, I am John ")
+}
+func (b Bot) SayTime() {
+	fmt.Println("Hello, I am John ")
+}
+
+// func (b BotEnglish) SayDate() {
+// 	fmt.Println("Hello, I am John ")
+// }
+// func (b BotEnglish) SayDay() {
+// 	fmt.Println("Hello, I am John ")
+// }
+// func (b BotEnglish) SayBye() {
+// 	fmt.Println("Hello, I am John ")
+// }
